a52c4b1fcf099bb5c2d1320611663b5f3066d0fd
Crazyorchid <zhugeruntao@gmail.com>
2022-07-28 00:25:57 +0930

    Rewrote the smart contract so we can have a cleaner version

--- contracts/acomosus_meta_nft.sol
+++ contracts/acomosus_meta_nft.sol
@@ -1,139 +1,107 @@
-// Contract based on https://docs.openzeppelin.com/contracts/3.x/erc721
-// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract AComosus is ERC721Enumerable, Ownable {
-    using Strings for uint256;
+contract AComosuss is ERC721Enumerable, Ownable {
+  using Strings for uint256;
 
-    bool public _isSaleActive = false;
-    bool public _revealed = false;
+  string public baseURI;
+  string public baseExtension = ".json";
+  uint256 public cost = 0.02 ether;
+  uint256 public maxSupply = 10000;
+  uint256 public maxMintAmount = 20;
+  bool public paused = false;
 
-    // Constants
-    uint256 public constant MAX_SUPPLY = 10;
-    uint256 public mintPrice = 0.3 ether;
-    uint256 public maxBalance = 1;
-    uint256 public maxMint = 1;
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _initBaseURI
+  ) ERC721(_name, _symbol) {
+    setBaseURI(_initBaseURI);
 
-    string baseURI;
-    string public notRevealedUri;
-    string public baseExtension = ".json";
+    mint(msg.sender, 5);
+  }
 
-    mapping(uint256 => string) private _tokenURIs;
+  // internal
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
 
-    constructor(string memory initBaseURI, string memory initNotRevealedUri)
-        ERC721("AComosus", "AC")
-    {
-        setBaseURI(initBaseURI);
-        setNotRevealedURI(initNotRevealedUri);
-    }
-//handle user mining
-    function mintAComosus(uint256 tokenQuantity) public payable {
-        require(
-            totalSupply() + tokenQuantity <= MAX_SUPPLY,
-            "Sale would exceed max supply"
-        );
-        require(_isSaleActive, "Sale must be active to mint AComosuss");
-        require(
-            balanceOf(msg.sender) + tokenQuantity <= maxBalance,
-            "Sale would exceed max balance"
-        );
-        require(
-            tokenQuantity * mintPrice <= msg.value,
-            "Not enough ether sent"
-        );
-        require(tokenQuantity <= maxMint, "Can only mint 1 tokens at a time");
+  // public
+  function mint(address _to, uint256 _mintAmount) public payable {
+    uint256 supply = totalSupply();
+    require(!paused);
+    require(_mintAmount > 0);
+    require(_mintAmount <= maxMintAmount);
+    require(supply + _mintAmount <= maxSupply);
 
-        _mintAComosus(tokenQuantity);
+    if (msg.sender != owner()) {
+      require(msg.value >= cost * _mintAmount);
     }
 
-    function _mintAComosus(uint256 tokenQuantity) internal {
-        for (uint256 i = 0; i < tokenQuantity; i++) {
-            uint256 mintIndex = totalSupply();
-            if (totalSupply() < MAX_SUPPLY) {
-                _safeMint(msg.sender, mintIndex);
-            }
-        }
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+      _safeMint(_to, supply + i);
     }
+  }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        virtual
-        override
-        returns (string memory)
-    {
-        require(
-            _exists(tokenId),
-            "ERC721Metadata: URI query for nonexistent token"
-        );
-
-        if (_revealed == false) {
-            return notRevealedUri;
-        }
-
-        string memory _tokenURI = _tokenURIs[tokenId];
-        string memory base = _baseURI();
-
-        // If there is no base URI, return the token URI.
-        if (bytes(base).length == 0) {
-            return _tokenURI;
-        }
-        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
-        if (bytes(_tokenURI).length > 0) {
-            return string(abi.encodePacked(base, _tokenURI));
-        }
-        // If there is a baseURI but no tokenURI, concatenate the tokenID to the baseURI.
-        return
-            string(abi.encodePacked(base, tokenId.toString(), baseExtension));
+  function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    for (uint256 i; i < ownerTokenCount; i++) {
+      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
     }
+    return tokenIds;
+  }
 
-    // internal
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseURI;
-    }
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
 
-    //only owner
-    function flipSaleActive() public onlyOwner {
-        _isSaleActive = !_isSaleActive;
-    }
+    string memory currentBaseURI = _baseURI();
+    return
+      bytes(currentBaseURI).length > 0
+        ? string(
+          abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension)
+        )
+        : "";
+  }
 
-    function flipReveal() public onlyOwner {
-        _revealed = !_revealed;
-    }
+  //only owner
+  function setCost(uint256 _newCost) public onlyOwner {
+    cost = _newCost;
+  }
 
-    function setMintPrice(uint256 _mintPrice) public onlyOwner {
-        mintPrice = _mintPrice;
-    }
+  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+    maxMintAmount = _newmaxMintAmount;
+  }
 
-    function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
-        notRevealedUri = _notRevealedURI;
-    }
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
 
-    function setBaseURI(string memory _newBaseURI) public onlyOwner {
-        baseURI = _newBaseURI;
-    }
+  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+    baseExtension = _newBaseExtension;
+  }
 
-    function setBaseExtension(string memory _newBaseExtension)
-        public
-        onlyOwner
-    {
-        baseExtension = _newBaseExtension;
-    }
+  function pause(bool _state) public onlyOwner {
+    paused = _state;
+  }
 
-    function setMaxBalance(uint256 _maxBalance) public onlyOwner {
-        maxBalance = _maxBalance;
-    }
-
-    function setMaxMint(uint256 _maxMint) public onlyOwner {
-        maxMint = _maxMint;
-    }
-
-    function withdraw(address to) public onlyOwner {
-        uint256 balance = address(this).balance;
-        payable(to).transfer(balance);
-    }
+  function withdraw() public payable onlyOwner {
+    require(payable(msg.sender).send(address(this).balance));
+  }
 }
